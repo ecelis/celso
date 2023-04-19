0638f7320f6a664b5ff7af3a9bfbe1598067dda2
Ernesto Celis <ernesto@celisdelafuente.net>
2023-04-19 16:40:05 -0600

    chore(register): Save images posted

--- web/pages/api/register.ts
+++ web/pages/api/register.ts
@@ -1,9 +1,44 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import useAxios from 'axios-hooks';
 import { HttpStatusCode } from 'axios';
+import * as fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
 type Data = {
   success: boolean,
+}
+
+function decodeBase64Image(dataString: string) {
+    const matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+    response = {
+        type: null,
+        data: null,
+    };
+  
+    // @ts-ignore
+    if (matches.length !== 3) {
+      return new Error('Invalid input string');
+    }
+  
+    // @ts-ignore
+    response.type = matches[1];
+    
+    // @ts-ignore
+    response.data = new Buffer(matches[2], 'base64');
+  
+    return response.data;
+}
+
+async function save(data: string) {
+    const imageBuffer = decodeBase64Image(data);
+    try {
+        const UPLOADS_DIR = '/Users/ecelis/xyz/congress/data/images/uploads/';
+        const fileName = UPLOADS_DIR + uuidv4() + '.jpg' 
+        // @ts-ignore
+        fs.writeFileSync(fileName, imageBuffer);
+    } catch (error) {
+      console.log(error);
+    }
 }
 
 export default function handler(
@@ -11,7 +46,7 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
     if(req.method === 'POST') {
-      console.log(req.body);
+      save(req.body.picture);
     } else {
       res.status(HttpStatusCode.BadRequest).json({ success: false });    
     }
