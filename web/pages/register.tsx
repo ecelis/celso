fc2f705b10db16a0ce6e027866cc8d5ead8f8ea6
Ernesto Celis <ernesto@celisdelafuente.net>
2023-04-19 14:27:00 -0600

    chore(register): Set default image quality

--- web/pages/register.tsx
+++ web/pages/register.tsx
@@ -5,6 +5,7 @@ import Webcam from 'react-webcam';
 import { Button, Container } from '@mui/material';
 
 const Register: NextPageWithLayout = () => {
+//   const [mirrored, setMirrored] = useState(false);
   const webCamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
 
@@ -14,16 +15,40 @@ const Register: NextPageWithLayout = () => {
     setImgSrc(imageSrc);
   }, [webCamRef]);
 
+  const retake = () => {
+    setImgSrc(null);
+  }
+
+  /*
+  The screenshotFormat prop allows us to specify the format of the screenshot.
+  The possible values for this prop are image/jpeg, image/png, and image/webp.
+  The default value is image/webp.
+
+  The screenshotQuality prop allows us to specify the quality of the screenshot.
+  The possible values for this prop range from 0 to 1, where 1 represents the
+  highest quality. The default value is 0.92.
+  */
   return (
+    <>
     <Container>
         {imgSrc ? ( <img src={imgSrc} alt="webcam" /> )
         : (
-        <>
-          <Webcam height={600} width={600} ref={webCamRef} />
-          <Button variant="contained" onClick={capture}>Tomar foto</Button>
-          </>
+          <Webcam
+            height={600}
+            width={600}
+            ref={webCamRef}
+            mirrored={true}
+            screenshotFormat='image/jpeg'
+            screenshotQuality={0.8} />
         )}
     </Container>
+    <Container>
+        {imgSrc ? 
+        (<Button variant="contained" onClick={retake}>Tomar otra vez</Button>)
+        :
+        (<Button variant="contained" onClick={capture}>Tomar foto</Button>)}
+    </Container>
+    </>
   );
 }
 
