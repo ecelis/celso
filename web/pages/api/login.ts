fc131477b516da75faf18099b2514b210a717b0c
Ernesto Celis <ernesto@celisdelafuente.net>
2023-05-09 21:23:53 -0600

    chore(login): Remove unneccesary code for saving picure on login

--- web/pages/api/login.ts
+++ web/pages/api/login.ts
@@ -2,8 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import axios, { HttpStatusCode } from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 import apiRoutes from '@/src/routes/routes';
-import decodeBase64Image from '@/src/helpers/base64';
-import * as fs from 'fs';
 
 type Data = {
   success: boolean,
@@ -18,21 +16,6 @@ export const config = {
   }
 }
 
-async function save(data: string, id: string) {
-  const imageBuffer = decodeBase64Image(data);
-  try {
-      const CELSO_SAMPLES = '/tmp/samples/' + id + '/';
-      if(!fs.existsSync(CELSO_SAMPLES)) {
-        await fs.mkdirSync(CELSO_SAMPLES)
-      }
-      const fileName = CELSO_SAMPLES + id.substring(0, 36) + '_' +  uuidv4() + '.jpg' 
-      // @ts-ignore
-      fs.writeFileSync(fileName, imageBuffer);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
@@ -40,9 +23,6 @@ export default async function handler(
     if(req.method === 'POST') {
       const id: string =  uuidv4();
       const {picture} = req.body;
-      // picture.forEach(async (element) => {
-        await save(picture, id);
-      // });
       const result = await axios.post(
         apiRoutes.MATCH,
         {
