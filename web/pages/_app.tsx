b6a35987f1d0b91646a3614c71e1aeec9eeaa7bf
Ernesto Celis <ernesto@celisdelafuente.net>
2023-05-09 19:13:10 -0600

    style(register): Fix Layout

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,26 +2,38 @@ import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react';
+import { theme } from '@/src/theme/theme';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { ThemeProvider } from '@mui/material';
+import createEmotionCache from '@/src/theme/createEmotionCache';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
+  Component: NextPageWithLayout,
 }
 
-export default function MyApp({ Component, pageProps: {session, ...pageProps} }: AppPropsWithLayout) {
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp({
+  Component,
+  pageProps: {session, ...pageProps} 
+}: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return (
     <SessionProvider
       session={session}
-    >{getLayout(<Component {...pageProps} />)}</SessionProvider>
+    >
+        <ThemeProvider theme={theme}>
+          {getLayout(<Component {...pageProps} />)}
+        </ThemeProvider>
+    </SessionProvider>
   );
 }
