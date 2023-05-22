a763182a77baf7039be8e2f9c4ca5c1c291ed911
Ernesto Celis <ernesto@celisdelafuente.net>
2023-05-21 20:10:29 -0600

    chore(env): Change setenv.sh to source from .env file

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-export MONGODB_URI=mongodb+srv://celso:Cr2sqNRUo0LjT7hp@cluster0.kim9c.mongodb.net/
-export MONGODB_DB=celso
-export CONGRESS_SECRET=v3rys3cr3t
-export CELSO_SAMPLES=3
-export CELSO_UNKNOWN=/tmp/unknown
-export EMAIL_SERVER_USER=apikey
-export EMAIL_SERVER_PASSWORD=changeme
-export EMAIL_SERVER_PORT=587
-export EMAIL_SERVER_HOST=smtp.sendgrid.net
-export EMAIL_FROM='congreso@congreso.com'
-export FACES_URL=http://localhost:5000
-export NEXTAUTH_URL=http://localhost:3000
-export FLASK_APP=faces/__main__.py
+for i in $(cat .env); do
+  export $i
+done
